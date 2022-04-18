b81d420b6663b1ac1d882c04e51654f5bc45f117
Auguste Rame <auguste.rame@gmail.com>
2022-04-18 08:08:05 -0400

    Implement Windows port iterator

--- src/serial.zig
+++ src/serial.zig
@@ -6,6 +6,7 @@ pub fn list() !PortIterator {
 }
 
 pub const PortIterator = switch (builtin.os.tag) {
+    .windows => WindowsPortIterator,
     .linux => LinuxPortIterator,
     else => @compileError("OS is not supported for port iteration"),
 };
@@ -15,6 +16,75 @@ pub const SerialPortDescription = struct {
     display_name: []const u8,
     driver: ?[]const u8,
 };
+
+const HKEY = std.os.windows.HANDLE;
+
+const WindowsPortIterator = struct {
+    const Self = @This();
+
+    key: HKEY,
+    index: u32,
+
+    name: [256:0]u8 = undefined,
+    name_size: u32 = 256,
+
+    data: [256]u8 = undefined,
+    filepath_data: [256]u8 = undefined,
+    data_size: u32 = 256,
+
+    pub fn init() !Self {
+        const HKEY_LOCAL_MACHINE = @intToPtr(HKEY, 0x80000002);
+        const KEY_READ = 0x20019;
+
+        var self: Self = undefined;
+        self.index = 0;
+        if (RegOpenKeyExA(HKEY_LOCAL_MACHINE, "HARDWARE\\DEVICEMAP\\SERIALCOMM\\", 0, KEY_READ, &self.key) != 0)
+            return error.WindowsError;
+
+        return self;
+    }
+
+    pub fn deinit(self: *Self) void {
+        RegCloseKey(self.key);
+        self.* = undefined;
+    }
+
+    pub fn next(self: *Self) !?SerialPortDescription {
+        defer self.index += 1;
+
+        self.name_size = 256;
+        self.data_size = 256;
+
+        return switch (RegEnumValueA(self.key, self.index, &self.name, &self.name_size, null, null, &self.data, &self.data_size)) {
+            0 => SerialPortDescription{
+                .file_name = try std.fmt.bufPrint(&self.filepath_data, "\\\\.\\{s}", .{self.data[0 .. self.data_size - 1]}),
+                .display_name = self.data[0 .. self.data_size - 1],
+                .driver = self.name[0..self.name_size],
+            },
+            259 => null,
+            else => error.WindowsError,
+        };
+    }
+};
+
+extern "advapi32" fn RegOpenKeyExA(
+    key: HKEY,
+    lpSubKey: std.os.windows.LPCSTR,
+    ulOptions: std.os.windows.DWORD,
+    samDesired: std.os.windows.REGSAM,
+    phkResult: *HKEY,
+) callconv(std.os.windows.WINAPI) std.os.windows.LSTATUS;
+extern "advapi32" fn RegCloseKey(key: HKEY) callconv(std.os.windows.WINAPI) std.os.windows.LSTATUS;
+extern "advapi32" fn RegEnumValueA(
+    hKey: HKEY,
+    dwIndex: std.os.windows.DWORD,
+    lpValueName: std.os.windows.LPSTR,
+    lpcchValueName: *std.os.windows.DWORD,
+    lpReserved: ?*std.os.windows.DWORD,
+    lpType: ?*std.os.windows.DWORD,
+    lpData: [*]std.os.windows.BYTE,
+    lpcbData: *std.os.windows.DWORD,
+) callconv(std.os.windows.WINAPI) std.os.windows.LSTATUS;
 
 const LinuxPortIterator = struct {
     const Self = @This();
@@ -481,6 +551,14 @@ const DCB = extern struct {
 
 extern "kernel32" fn GetCommState(hFile: std.os.windows.HANDLE, lpDCB: *DCB) callconv(std.os.windows.WINAPI) std.os.windows.BOOL;
 extern "kernel32" fn SetCommState(hFile: std.os.windows.HANDLE, lpDCB: *DCB) callconv(std.os.windows.WINAPI) std.os.windows.BOOL;
+
+test "iterate ports" {
+    var it = try list();
+    while (try it.next()) |port| {
+        _ = port;
+        // std.debug.print("{s} (file: {s}, driver: {s})\n", .{ port.display_name, port.file_name, port.driver });
+    }
+}
 
 test "basic configuration test" {
     var cfg = SerialConfig{
