06e45478e082313888080c02f0bc996cf19bc58b
FObersteiner <f.obersteiner@posteo.de>
2024-06-20 17:56:32 +0200

    add test step

--- build.zig
+++ build.zig
@@ -8,6 +8,15 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("src/serial.zig"),
     });
 
+    const unit_tests = b.addTest(.{
+        .root_source_file = b.path("src/serial.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    const run_unit_tests = b.addRunArtifact(unit_tests);
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_unit_tests.step);
+
     const echo_exe = b.addExecutable(.{
         .name = "serial-echo",
         .root_source_file = b.path("examples/echo.zig"),
